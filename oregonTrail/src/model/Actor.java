6619358e376a9f46ebe5343bd66b9c5e3ec45247
Kevin Thompson <kevindthompson@live.com>
2017-10-04 21:05:32 -0600

    Added the Party class and took out comment in the Actor class for verification

--- oregonTrail/src/model/Actor.java
+++ oregonTrail/src/model/Actor.java
@@ -1,22 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 
 import java.io.Serializable;
 import java.util.Objects;
 
-/**
- *
- * @author Kevin's Account
- */
-
 public class Actor implements Serializable{
-    
-    
-    //Something
     
     //Class instance variables
     private String name;
