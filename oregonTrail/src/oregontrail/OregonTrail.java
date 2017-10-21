629bf97aa708b3782890ec51ca87dfd2913084cb
Dan Ortega <Dan Ortega@DESKTOP-1L665D8>
2017-10-21 13:13:59 -0500

    Just playing around and getting the first running file working perfectly fine.

--- oregonTrail/src/oregontrail/OregonTrail.java
+++ oregonTrail/src/oregontrail/OregonTrail.java
@@ -13,6 +13,10 @@ import model.Supply;
 import model.Oxen;
 import model.Wagon;
 
+
+import model.TestoClass;
+import model.Actors;
+
 /**
  *
  * @author Kevin's Account
@@ -23,17 +27,25 @@ public class OregonTrail {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Party party = new Party();
-        party.setDistanceTraveled(1000);
-        party.setMapPositions(15);
-        // needs Oxen class
-//        ArrayList<Oxen> oxen = new ArrayList<>();
-//        Oxen o = new Oxen();
-//        oxen.add(o);
-//        party.setOxen(oxen);
-        party.setPace(3);
-        ArrayList<Actor> partyMembers = new ArrayList<>();
         
+        Oxen beast = new Oxen();
+        beast.setStrength(6);
+        System.out.println(beast.toString());
+        
+        Supply something = new Supply();
+        something.setType("Food");
+        something.setAmount(45);
+        System.out.println(something.toString());
+        
+        Wagon large = new Wagon();
+        large.setCarryingWeight(2500);
+        large.setSizeDescription("Largest");
+        large.setIsBroken(false);
+        large.setWagonWeight(800);
+        ArrayList<Supply> supplies = new ArrayList<Supply>();
+        supplies.add(something);
+        large.setSupplies(supplies);
+        System.out.println(large.toString());
         
         Actor actor = new Actor();
         actor.setName("Ricardo");
@@ -42,37 +54,27 @@ public class OregonTrail {
         actor.setGatheringSkill(5);
         actor.setMoney(1000);
         actor.setStamina(8);
+           
+        Party party = new Party();        
+        party.setDistanceTraveled(1000);
+        party.setMapPositions(15);
+        ArrayList<Oxen> oxen = new ArrayList<>();
+        oxen.add(beast);
+        party.setOxen(oxen);
+        party.setPace(3);
+        ArrayList<Actor> partyMembers = new ArrayList<>();
         partyMembers.add(actor);
         party.setPartyMembers(partyMembers);
         party.setPartyMoney(1500);
-        // needs wagon class
-//        Wagon wagon = new Wagon;
-//        party.setWagon(wagon);
+        party.setWagon(large);
         System.out.println(actor.toString());
-        System.out.println(party.toString());
-        
-        
+        System.out.println(party.toString());               
+               
         Player theBestOne = new Player();
         theBestOne.setName("Dancho");
         theBestOne.setParty(party);
         System.out.println(theBestOne.toString());
-        
-        
-        Supply something = new Supply();
-        something.setType("Food");
-        something.setAmount(45);
-        System.out.println(something.toString());
-        
-        Oxen beast = new Oxen();
-        beast.setStrength(6);
-        System.out.println(beast.toString());
-        
-        Wagon large = new Wagon();
-        large.setCarryingWeight(2500);
-        large.setSizeDescription("Largest");
-        large.setIsBroken(false);
-        large.setWagonWeight(800);
-        System.out.println(large.toString());
+
     }
-    
+        
 }
